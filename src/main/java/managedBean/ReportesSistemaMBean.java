25c31eae66031819ca51de1002b862cd26a0cd8a
paulpy <pauljourdanbaez@gmail.com>
2017-11-24 16:28:56 -0300

    Todos los reportes funcionando

--- src/main/java/managedBean/ReportesSistemaMBean.java
+++ src/main/java/managedBean/ReportesSistemaMBean.java
@@ -48,19 +48,35 @@ public class ReportesSistemaMBean implements Serializable {
 	private Map<String, Object> parametros;
 	private String tipoReporte;
 	private String urlreporte;
+	private Entidad entidadSelect;
 
 	public void inicializar() {
-		String contexto = FacesContext.getCurrentInstance().getExternalContext().getRequest().toString().substring(63,
-				67);
-		tipoReporte = contexto;
-		if (tipoReporte.equals("ripm")) {
-			entidadList = entidadService.listEntidad();
-			System.out.println("entro al entidad");
-		} else {
-			if (tipoReporte.equals("rcdp")) {
-				funcionarioTecList = funcionarioService.listFuncionarioTec();
-				System.out.println("entro al cambio de pieza");
+		if (!FacesContext.getCurrentInstance().isPostback()) {
+			if (tipoReporte != null) {
+				System.out.println("ya tiene contexto " + tipoReporte);
+			} else {
+				String contexto = FacesContext.getCurrentInstance().getExternalContext().getRequest().toString()
+						.substring(63, 67);
+				tipoReporte = contexto;
 			}
+		}
+		if (tipoReporte.equals("rins")) {
+			entidadList = entidadService.listEntidad();
+			System.out.println("entro a instalaciones");
+		}
+		if (tipoReporte.equals("rcdp")) {
+			funcionarioTecList = funcionarioService.listFuncionarioTec();
+			System.out.println("entro a cambio de pieza");
+		}
+		if (tipoReporte.equals("rtlc")) {
+			System.out.println("entro a contratos");
+		}
+		if (tipoReporte.equals("rtlp")) {
+			System.out.println("entro a piezas");
+		}
+		if (tipoReporte.equals("rman")) {
+			entidadList = entidadService.listEntidad();
+			System.out.println("entro a manteminietos");
 		}
 	}
 
@@ -82,6 +98,20 @@ public class ReportesSistemaMBean implements Serializable {
 					parametros = new HashMap<String, Object>();
 					parametros.put("fechainicio", fechaInicio);
 					parametros.put("fechafin", fechaFin);
+					if (tipoReporte.equals("rtlc")) {
+						FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+								"Se generara el Reporte de Contratos");
+						context.addMessage(null, m);
+						urlreporte = "/reportes/todosloscontratos.jrxml";
+						auditoriaClass.agregarAuditoria("Generando Reporte de Contratos", "vistareporte", usuario);
+					}
+					if (tipoReporte.equals("rtlp")) {
+						FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+								"Se generara el Reporte de Piezas");
+						context.addMessage(null, m);
+						urlreporte = "/reportes/todaslaspiezas.jrxml";
+						auditoriaClass.agregarAuditoria("Generando Reporte de Contratos", "vistareporte", usuario);
+					}
 					if (tipoReporte.equals("rcdp")) {
 						if (funcionarioSelect == null) {
 							FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
@@ -96,67 +126,43 @@ public class ReportesSistemaMBean implements Serializable {
 							urlreporte = "/reportes/cambiodepiezasmesfunc.jrxml";
 						}
 						auditoriaClass.agregarAuditoria("Generando Reporte Cambio de Pieza", "vistareporte", usuario);
-						System.out.println("entro al rcpd " + urlreporte);
+					}
+					if (tipoReporte.equals("rins")) {
+						if (entidadSelect == null) {
+							FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+									"Se generara el Reporte sin filtro de Entidad");
+							context.addMessage(null, m);
+							urlreporte = "/reportes/instalacionespormes.jrxml";
+						} else {
+							parametros.put("id", entidadSelect.getEntiIdEntidad());
+							FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+									"Se generara el Reporte con Entidad");
+							context.addMessage(null, m);
+							urlreporte = "/reportes/instalacionespormesporentidad.jrxml";
+						}
+						auditoriaClass.agregarAuditoria("Generando Reporte de Instalaciones", "vistareporte", usuario);
+					}
+					if (tipoReporte.equals("rman")) {
+						if (entidadSelect == null) {
+							FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+									"Se generara el Reporte sin filtro de Entidad");
+							context.addMessage(null, m);
+							urlreporte = "/reportes/mantenimientodelmes.jrxml";
+						} else {
+							parametros.put("id", entidadSelect.getEntiIdEntidad());
+							FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
+									"Se generara el Reporte con Entidad");
+							context.addMessage(null, m);
+							urlreporte = "/reportes/mantenimientodelmesentidad.jrxml";
+						}
+						auditoriaClass.agregarAuditoria("Generando Reporte de Mantenimiento", "vistareporte", usuario);
 					}
 					try {
-						return generadorDeReporte.generarReporte(urlreporte,
-								parametros);
+						return generadorDeReporte.generarReporte(urlreporte, parametros);
 					} catch (Exception e) {
 						// TODO: handle exception
 						System.out.println(e.toString());
 					}
-				}
-			}
-		}
-		return null;
-	}
-
-	public StreamedContent generarReporte(String usuario) {
-		if (fechaInicio == null) {
-			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Falta Ingresar Fecha Inicio");
-			context.addMessage(null, m);
-		} else {
-			if (fechaFin == null) {
-				FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Falta Ingresar Fecha Fin");
-				context.addMessage(null, m);
-			} else {
-				if ((fechaInicio.after(fechaFin)) || (fechaFin.before(fechaInicio))) {
-					System.out.println("Las fechas no estan en el orden Correcto");
-					FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Atencion",
-							"Las fechas no estan en el orden Correcto");
-					context.addMessage(null, m);
-				} else {
-					if (funcionarioSelect == null) {
-						FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
-								"Se generara el Reporte sin filtro de Funcionario");
-						context.addMessage(null, m);
-						try {
-							parametros = new HashMap<String, Object>();
-							parametros.put("FechaInicio", fechaInicio);
-							parametros.put("FechaFin", fechaFin);
-							return generadorDeReporte.generarReporte("/reportes/CambiodePiezasMes.jrxml", parametros);
-						} catch (Exception e) {
-							// TODO: handle exception
-							System.out.println(e.toString());
-						}
-					} else {
-						try {
-							parametros = new HashMap<String, Object>();
-							parametros.put("FechaInicio", fechaInicio);
-							parametros.put("FechaFin", fechaFin);
-							parametros.put("fechafin", fechaFin);
-							parametros.put("idpersona", funcionarioSelect.getPersona().getPersIdPersona());
-							return generadorDeReporte.generarReporte("/reportes/CambiodePiezasMesFunc.jrxml",
-									parametros);
-						} catch (Exception e) {
-							// TODO: handle exception
-							System.out.println(e.toString());
-						}
-						FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atencion",
-								"Se generara el Reporte ");
-						context.addMessage(null, m);
-					}
-					auditoriaClass.agregarAuditoria("Generando Reporte ", "vistareporte", usuario);
 				}
 			}
 		}
@@ -213,6 +219,14 @@ public class ReportesSistemaMBean implements Serializable {
 
 	public void setEntidadList(List<Entidad> entidadList) {
 		this.entidadList = entidadList;
+	}
+
+	public Entidad getEntidadSelect() {
+		return entidadSelect;
+	}
+
+	public void setEntidadSelect(Entidad entidadSelect) {
+		this.entidadSelect = entidadSelect;
 	}
 
 }
